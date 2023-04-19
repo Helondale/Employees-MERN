242e0d2f9c12ac2de744fbfea970ca2a88211965
Helondale <hatvaniadrienn99@gmail.com>
2023-04-19 21:27:53 +0200

    Updated

--- client/src/Pages/EmployeeList.jsx
+++ client/src/Pages/EmployeeList.jsx
@@ -6,23 +6,30 @@ const fetchEmployees = () => {
   return fetch("/api/employees").then((res) => res.json());
 };
 
-const deleteEmployee = (id) => {
-  return fetch(`/api/employees/${id}`, { method: "DELETE" }).then((res) =>
-    res.json()
-  );
-};
+
 
 const EmployeeList = () => {
   const [loading, setLoading] = useState(true);
   const [employees, setEmployees] = useState(null);
+  const [positionFilter, setPositionFilter] = useState("");
+  const [levelFilter, setLevelFilter] = useState("");
+  const deleteEmployee = (id) => {
+    return fetch(`/api/employees/${id}`, { method: "DELETE" })
+    .then((res) =>
+      setEmployees(employees.filter(x => id !== x._id))
+    );
+  };
+
 
   const handleDelete = (id) => {
     deleteEmployee(id);
-
-    setEmployees((employees) => {
-      return employees.filter((employee) => employee._id !== id);
-    });
   };
+
+  const handleClick = () => {
+  employees.map(employee => console.log(employee.position))  
+
+
+  }
 
   useEffect(() => {
     fetchEmployees()
@@ -36,7 +43,27 @@ const EmployeeList = () => {
     return <Loading />;
   }
 
-  return <EmployeeTable employees={employees} onDelete={handleDelete} />;
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Filter by Position"
+          value={positionFilter}
+          onChange={(e) => setPositionFilter(e.target.value)}
+          
+        />
+        <input
+          type="text"
+          placeholder="Filter by Level"
+          value={levelFilter}
+          onChange={(e) => setLevelFilter(e.target.value)}
+        />
+        <button onClick={handleClick}>Search</button>
+      </div><EmployeeTable employees={employees} onDelete={handleDelete}
+      />
+    </>
+    )
 };
 
 export default EmployeeList;
